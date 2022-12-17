70e0e474e43aec3086669041807820b35ccdca09
Deepak tiwari <atrivedi8988@gmail.com>
2022-12-17 14:21:05 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const JobPostModel = require("./JobPost.Schema")
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
